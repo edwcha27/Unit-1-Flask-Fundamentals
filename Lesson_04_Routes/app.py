1937bd7c8e0c6ceed4c194ea326efd0182d7dcd6
Ryan Chandler <gjyfghhg@gmail.com>
2025-09-30 09:52:29 -0400

    Commit

--- Lesson_04_Routes/app.py
+++ Lesson_04_Routes/app.py
@@ -2,34 +2,30 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '''
-            <h1>Dynamic Routes Demo</h1>
+
+@app.route("/")
+def home():
+    return """
+           <h1>Dynamic Routes Demo</h1>
 <h2>Try: These URLS</h2>
 <ul>
     <li><a href="/user/john">User Profile: john</a></li>
     <li><a href="/user/alice">User Profile: alice</a></li>
-    <li><a href=""></a><li>
-    <li><a href=""></a><li>
-    <li><a href=""></a><li>
-    <li><a href=""></a><li>
-    <li><a href=""></a><li>
-</ul>
-'''
+</ul> 
+"""
 
-@app.route('/user/<username>', methods=[''])
+@app.route('/user/<username>')
 def user_profile(username):
-    return '''
+    return f'''
      <h1>User Profile</h1>
  <p>Username: <strong>{username}</strong></p>
- <p>Profile TypeL {type(username).__name__}</p>
+ <p>Profile Type: {type(username).__name__}</p>
  <p>Welcome to {username}'s profile page!</p>
  <nav>
-    <a href="/">Back to Homepage</a>
+    <a href="/"> Back to Homepage</a>
     <a href="/user/alice">Alice</a>
     <a href="/user/bob">Bob</a>
- </nav>
+ </nav>    
 '''
 @app.route("/calc/<int:num1>/<operation>/<int:num2>")
 def calculator(num1, operation, num2):
@@ -37,15 +33,13 @@ def calculator(num1, operation, num2):
         '+' : num1 + num2,
         '-' : num1 - num2,
         '*' : num1 * num2,
-        '/' : num1 / num2 if num2 !=0 else 'Error: Division by zero!',
+        '/' : num1 / num2 if num2 != 0 else 'Error: Division by zero!'
     }
     if operation in operations:
         result = operations[operation]
         return f"{num1} {operation} {num2} = {result}"
     else:
         return f"Unknown operation!{operation}"
-        
-    # return f"{num1} {operation} {num2} = "
 
 @app.route("/temp/<str:unit>/<int:num>")
 def conversion(unit, num):
--- period4.py
+++
@@ -1,8 +0,0 @@
-itemName = input("enter the item name:")
-quantity = int(input(f"How many {itemName}?"))
-price = float(input("Enter the price"))
-
-if quantity > 0 and price > 0:
-    print(f"Total Price: {price} of {quantity} {itemName}s")
-else:
-    print("Error: Invalid Inputs!")
